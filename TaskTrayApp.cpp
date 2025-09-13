b2ea763f4e66e3fb352bf121ecf372eb10c83490
YukiOhira0416 <ohirayuki0416@gmail.com>
2025-09-14 01:54:11 +0900

    軽微な修正

--- TaskTrayApp.cpp
+++ TaskTrayApp.cpp
@@ -529,7 +529,7 @@ void TaskTrayApp::MonitorDisplayChanges() {
             sharedMemoryHelper.WriteSharedMemory("DISP_INFO_RE", "1");
             TaskTrayApp* appPtr = this;
             std::thread([appPtr]() {
-                Sleep(2000);
+                Sleep(1000);
                 SharedMemoryHelper helper(appPtr);
                 helper.WriteSharedMemory("DISP_INFO_RE", "0");
             }).detach();
