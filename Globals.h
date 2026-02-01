8e0c2486670987c86dca16032b1f652f2dc444ce
YukiOhira0416 <ohirayuki0416@gmail.com>
2026-02-01 16:08:12 +0900

    軽微な修正

--- Globals.h
+++ Globals.h
@@ -1,12 +1,4 @@
 ﻿#pragma once
 #include <vector>
 
-// グローバル変数の宣言
-
-#define SHAREDMEMORY_NAME_GPU L"GPU_INFO"
-#define SHAREDMEMORY_NAME_DISP L"DISP_INFO"
-#define REG_PATH_GPU L"SOFTWARE\\MyApp\\GPUInfo"
-#define REG_PATH_DISP L"SOFTWARE\\MyApp\\DISPInfo"
-#define REG_PATH_CAPTURE_TYPE L"SOFTWARE\\MyApp\\CaptureType"
-
 extern int serialNumberIndex;
