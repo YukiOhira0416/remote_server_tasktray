2b158b22453366163f50c8eadce55dab9a815e1a
YukiOhira <xylish.hyper.cool@icloud.com>
2026-02-15 21:53:54 +0900

    新しい開発環境に移行

--- ui_Main_UI.h
+++ ui_Main_UI.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'Main_UI.ui'
 **
-** Created by: Qt User Interface Compiler version 6.10.0
+** Created by: Qt User Interface Compiler version 6.10.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
